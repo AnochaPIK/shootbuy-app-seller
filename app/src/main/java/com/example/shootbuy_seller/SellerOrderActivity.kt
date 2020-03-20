ea6538cf63a3afc741825345ce5f942ace3c82bd
wattanachaiBoom <boom_sirisuk@hotmail.com>
2020-03-20 14:36:28 +0700

    Left UI

--- app/src/main/java/com/example/shootbuy_seller/SellerOrderActivity.kt
+++ app/src/main/java/com/example/shootbuy_seller/SellerOrderActivity.kt
@@ -6,25 +6,39 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.shootbuy_seller.Adapter.SellerOrderListAdapter
 import com.example.shootbuy_seller.Models.ProductOrder.SellerOrder
 import com.example.shootbuy_seller.Models.SellerOrderListData
 import com.example.shootbuy_seller.Services.GetSellerOrderList
 
-class SellerOrderActivity : AppCompatActivity() {
+class SellerOrderActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener {
+
+
     private var sellOrderRecyclerView: RecyclerView? = null
+    private var swipeRefreshLayout: SwipeRefreshLayout? = null
     private var pref: SharedPreferences? = null
     private var sellerUuid: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_seller_order)
-
         sellOrderRecyclerView = findViewById(R.id.sellerOrderRecyclerView)
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout)
+        swipeRefreshLayout!!.setOnRefreshListener(this)
         pref = getSharedPreferences("SP_Seller_DATA", Context.MODE_PRIVATE)
         sellerUuid = pref!!.getString("UUID", "")
 
+        setDataList()
+    }
+
+    private fun setDataList() {
+        swipeRefreshLayout!!.isRefreshing = true
         var url = IPAddress.ipAddress + "product-order/getSellerOrderList/" + sellerUuid
         GetSellerOrderList(listener).execute(url)
+    }
+
+    override fun onRefresh() {
+        setDataList()
     }
 
     override fun onStart() {
@@ -35,6 +49,8 @@ class SellerOrderActivity : AppCompatActivity() {
 
     var listener = object : GetSellerOrderList.getDataComplete {
         override fun getDataComplete(sellerOrderList: List<SellerOrder>) {
+            swipeRefreshLayout!!.isRefreshing = false
+
             var sellerOrderListData = ArrayList<SellerOrderListData>()
             for (i in sellerOrderList.indices) {
                 var address =
